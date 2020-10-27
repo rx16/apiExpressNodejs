21754a2f471a5dde0bb9ccc02abe36bd1ea249cf
Carlos Fernandez <carlosph79@gmail.com>
2020-10-26 21:11:08 -0300

    elimine el archivo routes y pase los metodos a app.js

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 //Requerimientos necesarios para poner en funcionamiento el servidor
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("../apiExpress/routes/routes");
+//const routes = require("../apiExpress/routes/routes");
 const PORT = process.env.PORT || 3001;
+const pool = require("../data/config");
 const app = express();
 
 //Uso body-parser para el parseo de objetos json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Metodo GET a la raíz de la API
-routes(app);
-/*
-app.get("/", (request, response) => {
-  console.log(`URL: ${request.url} `);
-  response.send({ message: "API REST on Node.js Server" });
+//Listar todos los usuarios
+app.get("/users", (request, response) => {
+  pool.query("SELECT * FROM users", (error, result) => {
+    if (error) throw error;
+    response.send(result);
+  });
 });
-*/
+//Listar datos de un usuario pasando su id como parametro en la url
+app.get("/users/:id", (request, response) => {
+  const id = request.params.id;
+  pool.query("SELECT * FROM users WHERE id = ?", id, (error, result) => {
+    if (error) throw error;
+    response.send(result);
+  });
+});
+//Añadir un nuevo usuario
+app.post("/users", (request, response) => {
+  pool.query("INSERT INTO users SET ?", request.body, (error, result) => {
+    if (error) throw error;
+    response.status(201).send(`Usuario ingresado con id: ${result.insertId}`);
+  });
+});
+//Editar los datos de un usuario
+app.put("/users/:id", (request, response) => {
+  const id = request.params.id;
+  pool.query(
+    "UPDATE users SET ? WHERE id = ?",
+    [request.body, id],
+    (error, result) => {
+      if (error) throw error;
+      response.send("Se editaron los datos del usuario");
+    }
+  );
+});
 
 //Pone el servidor en escucha en el puerto 3001
 const server = app.listen(PORT, (error) => {
