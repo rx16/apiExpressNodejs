191f3da49da63cc1c1700381bb32b8f5c255ade8
Carlos Fernandez <carlosph79@gmail.com>
2020-10-27 19:01:18 -0300

    Api funcionando correctamente en https://rx16-apiexpress.herokuapp.com/

--- app.js
+++ app.js
@@ -11,57 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //LLama al enrutador
 routes(app);
-/*
-//Configuracion para base de datos de heroku
-const config = {
-  host: "us-cdbr-east-02.cleardb.com",
-  user: "bdb1fd02f5388a",
-  password: "512f33a3",
-  database: "heroku_7035080c4c6f3b2",
-  ssl:0
-};
-const sql = mysql.createConnection(config);
-
-//Listar todos los usuarios
-app.get("/users", (request, response) => {
-  // response.send(users);
-  sql.query("SELECT * FROM users", (error, result) => {
-    if (error) throw error;
-    response.send(result);
-  });
-});
-//Listar datos de un usuario pasando su id como parametro en la url
-app.get("/users/:id", (request, response) => {
-  const id = request.params.id;
-  // response.send(users[id]);
-  sql.query("SELECT * FROM users WHERE id = ?", id, (error, result) => {
-    if (error) throw error;
-    response.send(result);
-  });
-});
-
-//Añadir un nuevo usuario
-app.post("/users", (request, response) => {
-  sql.query("INSERT INTO users SET ?", request.body, (error, result) => {
-    if (error) throw error;
-    response.status(201).send(`Usuario ingresado con id: ${result.insertId}`);
-  });
-});
-//Editar los datos de un usuario
-app.put("/users/:id", (request, response) => {
-  const id = request.params.id;
-  sql.query(
-    "UPDATE users SET ? WHERE id = ?",
-    [request.body, id],
-    (error, result) => {
-      if (error) throw error;
-      response.send("Se editaron los datos del usuario");
-    }
-  );
-});
-*/
-//Pone el servidor en escucha en el puerto 3001
-const server = app.listen(PORT, (error) => {
-  if (error) return console.log(`Error: ${error}`);
+//Pone el servidor en escucha en el puerto asignado
+const server = app.listen(PORT, () => {
   console.log(`Server listening on ${server.address().port}`);
 });
